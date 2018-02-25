13e70a5c03930c29c890d545c882b3d3fcdb79bd
Steve Duncan <SteveDuncan@comcast.net>
2018-02-25 11:32:48 -0800

    Fixed ave rating calc to account for no ratings

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -3,179 +3,194 @@ var router = express.Router();
 const moment = require('moment');
 
 //models
-const {User} = require('./../models/user');
-const {Article} = require('./../models/article');
-const {UserReviews} = require('./../models/user_reviews');
+const { User } = require('./../models/user');
+const { Article } = require('./../models/article');
+const { UserReviews } = require('./../models/user_reviews');
 
 
 
-const {auth} = require('./../middleware/auth');
+const { auth } = require('./../middleware/auth');
 
 //GET
-router.get('/',(req,res)=>{
-    Article.find().sort({_id:'asc'}).limit(10).exec((err,doc)=>{
+// router.get('/',(req,res)=>{
 
-        doc.forEach(function(item){
+//     Article.find().sort({_id:'asc'}).limit(10).exec((err,doc)=>{
+//         if(err) return res.status(400).send(err);
+//         res.render('home',{
+//             articles:doc
+//         })
+//     })
 
-            var pipeline = [
-                {$match: {"titlePost":item.title}},
-                {$group: {_id: null,aveRating: {$avg: "$rating"}}},
-                {$project: {_id: 0,aveRating:1}}
-            ]
+    
+// })
 
-            UserReviews.aggregate(pipeline,function(err,result){
-                if(err) {console.log(err)}
-
-                const ave = (Object.values(result[0]))[0]
-                // console.log(ave)
-                //How to get the ave val out of the function and into the returned doc??
-                
-                item["aveRating"] = ave;
-                Article.aggregate([{$addFields: {newField: "WTF??"}}])
-                
-            })
-            
-            console.log(item)
-        })
+function getAve(title, callback) {
+    const pipeline = [
+        { $match: { "titlePost": title } },
+        { $group: { _id: null, rating: { $avg: "$rating" } } },
+        { $project: { _id: 0, review: 1, rating: 1 } }
+    ]
+    UserReviews.aggregate(pipeline, function (err, result) {
+        if (err) { console.log(err) }
+        if (Array.isArray(result) && result.length) {
+            // console.log(result)
+            const ave = (Object.values(result[0]))[0]
+            // console.log("Ave rating: " + ave)
+            callback(ave);
+        } else {
+            callback(0)
+        }
+    })
         
-        return res.render('home',{
-            articles:doc
-            
+}
+
+router.get('/', (req, res) => {
+    Article.find().sort({ _id: 'asc' }).limit(10).exec((err, doc) => {
+        if(err) return res.status(400).send(err);
+        
+        doc.forEach(function (item) {
+            // console.log(item.title)
+            getAve(item.title, function (result) {
+                console.log(item.title + ": " + result)
+                item["aveRating"] = result;
+            })          
+        })
+        return res.render('home', {
+            articles: doc
         })
     })
-    
-    
 })
 
-router.get('/register',auth,(req,res)=>{
-    if(req.user) return res.redirect('/dashboard');
+router.get('/register', auth, (req, res) => {
+    if (req.user) return res.redirect('/dashboard');
     res.render('register')
 })
 
-router.get('/login',auth,(req,res)=>{
-    if(req.user) return res.redirect('/dashboard');
+router.get('/login', auth, (req, res) => {
+    if (req.user) return res.redirect('/dashboard');
     res.render('login');
 })
 
-router.get('/games/:id',auth,(req,res)=>{
+router.get('/games/:id', auth, (req, res) => {
 
     let addReview = req.user ? true : false;
 
-    Article.findById(req.params.id,(err,article)=>{
-        if(err) res.status(400).send(err);
+    Article.findById(req.params.id, (err, article) => {
+        if (err) res.status(400).send(err);
 
-        UserReviews.find({'postID':req.params.id}).exec((err,userReviews)=>{
-            res.render('article',{
-                date:moment(article.createdAt).format('MM/DD/YY'),
+        UserReviews.find({ 'postID': req.params.id }).exec((err, userReviews) => {
+            res.render('article', {
+                date: moment(article.createdAt).format('MM/DD/YY'),
                 article,
-                review:addReview,
+                review: addReview,
                 userReviews
             })
         })
 
- 
+
     })
 })
 
-router.get('/dashboard',auth,(req,res)=>{
-    if(!req.user) return res.redirect('/login');
-
-    res.render('dashboard',{
-        dashboard:true,
+router.get('/dashboard', auth, (req, res) => {
+    if (!req.user) return res.redirect('/login');
+    
+    res.render('dashboard', {
+        dashboard: true,
+        username: req.user.username,
         isAdmin: req.user.role === 1 ? true : false
     });
 })
 
-router.get('/dashboard/articles',auth,(req,res)=>{
-    if(!req.user) return res.redirect('/login');
-    res.render('admin_articles',{
-        dashboard:true,
+router.get('/dashboard/articles', auth, (req, res) => {
+    if (!req.user) return res.redirect('/login');
+    res.render('admin_articles', {
+        dashboard: true,
         isAdmin: req.user.role === 1 ? true : false
     });
 })
 
-router.get('/dashboard/reviews',auth,(req,res)=>{
+router.get('/dashboard/reviews', auth, (req, res) => {
 
-    if(!req.user) return res.redirect('/login');
-    UserReviews.find({'ownerID':req.user._id}).exec((err,userReviews)=>{
+    if (!req.user) return res.redirect('/login');
+    UserReviews.find({ 'ownerID': req.user._id }).exec((err, userReviews) => {
 
-        
-        res.render('admin_reviews',{
-            dashboard:true,
+
+        res.render('admin_reviews', {
+            dashboard: true,
             isAdmin: req.user.role === 1 ? true : false,
             userReviews
         });
     })
 })
 
-router.get('/dashboard/logout',auth,(req,res)=>{
+router.get('/dashboard/logout', auth, (req, res) => {
 
-    req.user.deleteToken(req.token,(err,user)=>{
-        if(err) return res.status(400).send(err);
+    req.user.deleteToken(req.token, (err, user) => {
+        if (err) return res.status(400).send(err);
         res.redirect('/');
     })
 })
 
 //POST
-router.post('/api/register',(req,res)=>{
+router.post('/api/register', (req, res) => {
 
     const user = new User(req.body);
 
-    user.save((err,doc)=>{
-        if(err) return res.status(400).send(err);
+    user.save((err, doc) => {
+        if (err) return res.status(400).send(err);
 
-        user.generateToken((err,user)=>{
-            if(err) return res.status(400).send(err);
-            res.cookie('auth',user.token).send('ok');
+        user.generateToken((err, user) => {
+            if (err) return res.status(400).send(err);
+            res.cookie('auth', user.token).send('ok');
         })
     })
 })
 
-router.post('/api/login',(req,res)=>{
-    
-    User.findOne({'email':req.body.email},(err,user)=>{
-        if(!user) return res.status(400).json({message:'Auth failed, wrong email'})
+router.post('/api/login', (req, res) => {
 
-        user.comparePassword(req.body.password, function(err, isMatch) {
-            if(err) throw err;
-            if(!isMatch) return res.status(400).json({message:'Auth failed, wrong password'});
+    User.findOne({ 'email': req.body.email }, (err, user) => {
+        if (!user) return res.status(400).json({ message: 'Auth failed, wrong email' })
 
-            user.generateToken((err,user)=>{
-                if(err) return res.status(400).send(err);
-                res.cookie('auth',user.token).send('ok');
+        user.comparePassword(req.body.password, function (err, isMatch) {
+            if (err) throw err;
+            if (!isMatch) return res.status(400).json({ message: 'Auth failed, wrong password' });
+
+            user.generateToken((err, user) => {
+                if (err) return res.status(400).send(err);
+                res.cookie('auth', user.token).send('ok');
             })
         })
     })
 })
 
-router.post('/api/add_article',auth,(req,res)=>{
-    
+router.post('/api/add_article', auth, (req, res) => {
+
     const article = new Article({
-        ownerUsername:req.user.username,
-        ownerID:req.user._id,
-        title:req.body.title,
-        review:req.body.review,
-        rating:req.body.rating
+        ownerUsername: req.user.username,
+        ownerID: req.user._id,
+        title: req.body.title,
+        overview: req.body.review,
+        rating: req.body.rating
     });
 
-    article.save((err,doc)=>{
-        if(err) res.status(400).send(err);
+    article.save((err, doc) => {
+        if (err) res.status(400).send(err);
         res.status(200).send();
     })
 })
 
-router.post('/api/user_review',auth,(req,res)=>{
+router.post('/api/user_review', auth, (req, res) => {
     const userReview = new UserReviews({
-        postID:req.body.id,
+        postID: req.body.id,
         ownerUsername: req.user.username,
-        ownerID:req.user._id,
-        titlePost:req.body.titlePost,
-        review:req.body.review,
-        rating:req.body.rating
+        ownerID: req.user._id,
+        titlePost: req.body.titlePost,
+        review: req.body.review,
+        rating: req.body.rating
     });
 
-    userReview.save((err,doc)=>{
-        if(err) return res.status.send(err);
+    userReview.save((err, doc) => {
+        if (err) return res.status.send(err);
         res.status(200).send();
     })
 })
